7c22a2db2c34c440fc711bbb44da7b047b99c024
FireRed222 <fireredgg2000@gmail.com>
2025-06-06 23:04:30 +0500

    The same App.jsx fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/posts' element={<GetPosts/>}/>
-        <Route path='/posts/:id' element={<GetPostsById/>}/>
       </Routes>
 
     </>
